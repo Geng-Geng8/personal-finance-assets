d495ecbe1c1798865aa28b131a5b1dd2e6452851
Geng-Geng8 <70758896+Geng-Geng8@users.noreply.github.com>
2026-09-02 16:25:01 -0400

    feat(config): bind Version 23 Web App endpoint URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 window.FINANCE_APP_CONFIG = Object.freeze({
   environment: "production",
-  webAppEndpointUrl: "",
+  webAppEndpointUrl: "https://script.google.com/macros/s/AKfycbxoRJ6dv8RdrZNtR_IjGkgCc_J6sbLyffsxt9xEiYJLjDGeWsJ0o73HYLcjTnJX3ajQ/exec",
   oauthClientId: "522582558662-a8vk7etqodg192sl68fe0rp1svo1jnkj.apps.googleusercontent.com",
   apiExecutableDeploymentId: "AKfycbxKhDN9cPwe7sy_CD8FjUiEMWQdL0buAcxMgp4CaRSxL7sXbsDeIv0N8jusxeO8Vk1o",
   oauthScope: "https://www.googleapis.com/auth/spreadsheets"
